361b8e95c1ab7c4f79a917cdf9664bd247bf1eb1
JackLiu <gamelife159@gmail.com>
2014-11-23 11:29:58 +0800

    update, #4

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,16 @@
+
+var post = [{
+	"title": "您好",
+	"content": "今天星期天"
+
+},
+{
+	"title": "Hello",
+	"content": "Today is Sunday"
+}];
+
 exports.list = function(req, res){
+	res.send(post);
 };
 
 exports.create = function(req, res){
